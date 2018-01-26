c444c2ff6e4f6f66841ee1acbd61a278b8303869
MrNeko <slavapozitif@yandex.ru>
2018-01-26 19:17:54 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Proekt-1</title>
+        <link href="zapis.html"/>
+        <link href="style.css" rel="stylesheet" type="text/css"/>
+        <meta name="viewport" content="width=device-width">
+    
+    </head>
+    <body>
+    <nav>
+        <style>
+            @viewport {
+    width: device-width;
+    zoom: 1;
+    max-zoom: 2;
+   }
+        </style>
+        <div id="block-topleft">
+              <h1 style="text-align:center;
+                 margin-top: 20%;
+                 font-size:100px"
+                 >
+                 
+                  <a href="zapis.html">Записаться</a>
+                 
+              </h1>
+        
+        </div>     
+        <div id="block-topright">
+            <h1 style="text-align:center;
+                 margin-top: 20%;
+                 font-size:100px"
+                 >
+                <a href="index.html">Посмотреть график</a>
+                 
+            </h1>
+            </div>
+        <div id="block-left">
+        <h1 style="text-align:center;
+                 margin-top: 20%;
+                 font-size:100px"
+                 >
+               <a href="index.html">Проверка талона</a>
+                 
+            </h1>
+            </div>
+        <div id="block-right">
+       <h1 style="text-align:center;
+                 margin-top: 20%;
+                 font-size:100px"
+                 >
+                <a href="index.html">Помощь</a>
+                 
+            </h1>
+        
+        
+        </div>     
+    </nav>
+    </body>
+</html>
